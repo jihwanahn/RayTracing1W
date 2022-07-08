ecc4c3e0bb3900dc046c94ea785bef45d027a788
Jihwan Peter Ahn <33449025+jihwanahn@users.noreply.github.com>
2022-07-08 23:43:38 +0900

    ppm edit

--- inOneWeekend/inOneWeekend.cpp
+++ inOneWeekend/inOneWeekend.cpp
@@ -8,10 +8,8 @@ int main() {
     const int image_height = 256;
 
     // Render
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
@@ -24,4 +22,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::cerr << "\nDone.\n";
 }
